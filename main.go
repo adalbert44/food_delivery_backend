c91120e3ae8218a20cc63dabd6d3592eb90e009d
Adalbert Makarovych <amakarovych-ua@memsql.com>
2020-09-06 23:20:39 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+)
+
+type Type struct {
+	Id int64 `json:"id"`
+	Name string `json:"name"`
+}
+
+func getDBConnection() (*sqlx.DB, error){
+	connParams := strings.Join([]string{
+		"parseTime=true",
+		"interpolateParams=true",
+		"timeout=10s",
+		"collation_server=utf8_general_ci",
+		"sql_select_limit=18446744073709551615",
+		"compile_only=false",
+		"enable_auto_profile=false",
+		"sql_mode='STRICT_ALL_TABLES,ONLY_FULL_GROUP_BY'",
+	}, "&")
+
+	defaultConfigString := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/information_schema?%s",
+		os.Getenv("MEMSQL_USER"),
+		os.Getenv("MEMSQL_PASSWORD"),
+		os.Getenv("MEMSQL_HOST"),
+		os.Getenv("MEMSQL_PORT"),
+		connParams,
+	)
+
+	return sqlx.Open("mysql", defaultConfigString)
+}
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	router.GET("/type", func(c *gin.Context) {
+		conn, err := getDBConnection()
+		if err != nil {
+			c.String(404, fmt.Sprintf("%v", err))
+			return
+		}
+
+		rows, err := conn.Query("SELECT * FROM FoodDelivery.type")
+		if err != nil {
+			c.String(404, fmt.Sprintf("%v", err))
+			return
+		}
+
+		types := make([]Type, 0)
+		for rows.Next() {
+			curType := Type {}
+			err := rows.Scan(&curType.Id, &curType.Name)
+			if err != nil {
+				c.String(404, fmt.Sprintf("%v", err))
+				return
+			}
+			types = append(types, curType)
+		}
+		c.JSON(200, types)
+	})
+
+	err := router.Run(":" + port)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
